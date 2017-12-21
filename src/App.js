f80f2d25c571cb3141fec2bdef5d428c3951a5e8
Enrico Bernardo <ebernardo@gmail.com>
2017-12-20 18:40:57 -0800

    more tweaks

--- src/App.js
+++ src/App.js
@@ -25,59 +25,50 @@ class App extends Component {
     return a.map(item => { return item; });
   }
 
-  removeFriend = id => {
-    console.log("hi there: ", id.target);
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  }
-
   // method for handling image click events
   friendClick = event => {
-      console.log("hello: ", event);
-      const currentImage = event.target.alt;
-      const alreadyClicked = this.state.clickedImages.indexOf(currentImage) > -1;
+    console.log("hello: ", event);
+    const currentImage = event.target.alt;
+    const alreadyClicked = this.state.clickedImages.indexOf(currentImage) > -1;
 
-      // if you click on an image more than once, you lose and the score resets to 0, the clickedImages are reset, and the images reshuffle
-      if (alreadyClicked) {
-          alert("You lost!");
-          this.setState({
+    // actions if you click on an image more than once: lose alert, score resets to 0, images reshuffle
+    if (alreadyClicked) {
+        alert("You lost!");
+        this.setState({
+            friends: this.shuffleCards(friends),
+            clickedImages: [],
+            score: 0
+        });
+    } else {
+      // first image click scores a point, selected image stored in array, and the images reshuffle
+      this.setState(
+          {
+              friends: this.shuffleCards(friends),
+              clickedImages: this.state.clickedImages.concat(currentImage),
+              score: this.state.score + 1,
+          },
+
+        () => {
+          // methods called with each click: resets values when you win and keeps best score
+          if(this.state.bestScore < this.state.score) {
+            this.setState(
+              {
+                bestScore: this.state.bestScore + 1
+              }
+            );
+          }
+
+          if (this.state.score === 12) {
+            alert("You win!");
+            this.setState({
               friends: this.shuffleCards(friends),
               clickedImages: [],
               score: 0
-          });
-      } else {
-          // else you score a point, the selected image is stored in the clickedImages array, and the images reshuffle
-          this.setState(
-              {
-                  friends: this.shuffleCards(friends),
-                  clickedImages: this.state.clickedImages.concat(currentImage),
-                  score: this.state.score + 1,
-              },
-
-              // methods called all the time; resets values when you win and keeps best score
-              () => {
-
-                if(this.state.bestScore < this.state.score) {
-                  this.setState(
-                    {
-                      bestScore: this.state.bestScore + 1
-                    }
-                  );
-                }
-
-                if (this.state.score === 12) {
-                    alert("You win!");
-                    this.setState({
-                        friends: this.shuffleCards(friends),
-                        clickedImages: [],
-                        score: 0
-                    });
-                }
-              }
-          );
-      }
+            });
+          }
+        }
+      );
+    }
   }
 
   // Map over this.state.friends and render a FriendCard component for each friend object
@@ -104,6 +95,6 @@ class App extends Component {
     );
   }
 
-}
+};
 
 export default App;
